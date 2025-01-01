49226a23043a69c21f368e182690ce88b8a02bb5
Khachatur <37556258+Khachatur-Ogannisyan@users.noreply.github.com>
2025-01-01 23:34:19 +0300

    Update telegram_google_bot.py

--- telegram_google_bot.py
+++ telegram_google_bot.py
@@ -28,7 +28,7 @@ def main():
     updater = Updater("8061703889:AAHWhFDcMl9Shmqy_EBT4xn9msB95BDcu3o", use_context=True)  # Замените ВАШ_ТОКЕН на токен бота
     dp = updater.dispatcher
 
-    dp.add_handler(MessageHandler(filters.text & ~filters.command, handle_message))
+    dp.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
     updater.start_polling()
     updater.idle()
